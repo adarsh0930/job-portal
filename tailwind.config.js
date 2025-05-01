a03020f7ab720d648644776f26e4a49ef65b6fdf
Adarsh Singh <adarshsingh0930@gmail.com>
2025-05-01 12:54:32 +0530

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+      container: {
+        center: true,    // automatically applies mx-auto
+        padding: {
+          DEFAULT: '1rem', // px-4
+          sm: '2rem',      // px-8 at sm and up
+          lg: '4rem',
+          xl: '5rem',
+        },
+      },
+    },
+  }
