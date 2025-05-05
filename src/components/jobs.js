fbcf271d392869d1eac5557c43ef52c82a682628
Adarsh Singh <adarshsingh0930@gmail.com>
2025-05-05 14:28:42 +0530

    adds onClick & activeTags feature

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -1,13 +1,16 @@
 import React from "react";
 import JobCard from "./jobCard";
-import jobData from "../data.json"
 
-
-export default function Jobs() {
+export default function Jobs({ jobs, onTagClick, activeTags }) {
   return (
     <div className="min-h-screen bg-teal-50 p-6 space-y-6">
-      {jobData.map((job, index) => (
-        <JobCard key={index} {...job} />
+      {jobs.map((job, index) => (
+        <JobCard
+          key={index}
+          {...job}
+          onTagClick={onTagClick}
+          activeTags={activeTags}
+        />
       ))}
     </div>
   );
